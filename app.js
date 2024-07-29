79fe19782149f18c4b855d5b61c297da323c5315
Karan Balkar <karanbalkar@gmail.com>
2024-07-29 23:00:36 +0530

    Updated

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World test 2 new from Argo CD!'))
+app.get('/', (req, res) => res.send('Hello World test 3 new from Argo CD!'))
 app.listen(3000, () => console.log('Server ready'))
